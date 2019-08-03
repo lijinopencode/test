79650a19172ec9014f70f5db95fa071fd3ea0886
lijinopencode <1050266856>
2019-08-04 01:21:58 +0800

    1

--- ConsoleApp1/消息传递/Singlete.cs
+++ ConsoleApp1/消息传递/Singlete.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//test
 namespace 消息传递
 {
     public class Singleton<T> where T : class, new()
